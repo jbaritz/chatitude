09f53a0791630e1bc7c0678ce339b05783122c1c
Julia Baritz <jbaritz@gmail.com>
2014-12-16 14:55:32 -0600

    small javascript changes

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -90,62 +90,62 @@ $('form.chatform').on('submit', function (e) { //new chat submit
 
   }) //end signin click
 
-$('form.signupform').on('submit', function (e) { //signupform
-  e.preventDefault()
-  var requestBody = {}
-  requestBody.username = $('[name=username]', this).val()
-  requestBody.password =  $('[name=password]', this).val()
-  var url = "http://chat.api.mks.io/signup"
+// $('form.signupform').on('submit', function (e) { //signupform
+//   e.preventDefault()
+//   var requestBody = {}
+//   requestBody.username = $('[name=username]', this).val()
+//   requestBody.password =  $('[name=password]', this).val()
+//   var url = "http://chat.api.mks.io/signup"
 
-  $.post(url, requestBody)
-    .done(function(newUser) {
-      var $labeldiv = $('<div>')
-      $labeldiv.html("You have successfully registered!")
-       $('form.signupform').append($labeldiv)
+//   $.post(url, requestBody)
+//     .done(function(newUser) {
+//       var $labeldiv = $('<div>')
+//       $labeldiv.html("You have successfully registered!")
+//        $('form.signupform').append($labeldiv)
 
-    })
-    .fail(function(error) {
-      console.log("Invalid POST request:", error.responseText)
-      var errorObj = JSON.parse(error.responseText)
-      for (var fieldName in errorObj) {
-      console.log(fieldName, errorObj[fieldName])
-      var errorText = errorObj[fieldName].join(';')
-      var $errorDiv = $('<div>').addClass('error')
-      $errorDiv.html(fieldName + " - " + errorText)
-      $('form.signupform .signuperror').append($errorDiv)
-      }
-    })
-	}) // end signup post
-$('form.signinform').on('submit', function (e) { //signinform
-  e.preventDefault()
-  var requestBody = {}
-  requestBody.username = $('[name=username]', this).val()
-  requestBody.password =  $('[name=password]', this).val()
-  var url = "http://chat.api.mks.io/signin"
+//     })
+//     .fail(function(error) {
+//       console.log("Invalid POST request:", error.responseText)
+//       var errorObj = JSON.parse(error.responseText)
+//       for (var fieldName in errorObj) {
+//       console.log(fieldName, errorObj[fieldName])
+//       var errorText = errorObj[fieldName].join(';')
+//       var $errorDiv = $('<div>').addClass('error')
+//       $errorDiv.html(fieldName + " - " + errorText)
+//       $('form.signupform .signuperror').append($errorDiv)
+//       }
+//     })
+// 	}) // end signup post
+// $('form.signinform').on('submit', function (e) { //signinform
+//   e.preventDefault()
+//   var requestBody = {}
+//   requestBody.username = $('[name=username]', this).val()
+//   requestBody.password =  $('[name=password]', this).val()
+//   var url = "http://chat.api.mks.io/signin"
 
-  $.post(url, requestBody)
-    .done(function(response) {
-      var $labeldiv = $('<div>')
-      $labeldiv.html("You have successfully logged in!")
-      $('form.signinform').append($labeldiv)
+//   $.post(url, requestBody)
+//     .done(function(response) {
+//       var $labeldiv = $('<div>')
+//       $labeldiv.html("You have successfully logged in!")
+//       $('form.signinform').append($labeldiv)
      
-      localStorage.setItem('apiToken', response['apiToken'])
-      localStorage.setItem('currentUser', requestBody['username'])
-      console.log("localstorage:",localStorage)
-      $('.loggedin').append('Welcome, ' + localStorage.getItem("currentUser"))
-    })
-    .fail(function(error) {
-      console.log("Invalid POST request:", error.responseText)
-      var errorObj = JSON.parse(error.responseText)
-      for (var fieldName in errorObj) {
-      console.log(fieldName, errorObj[fieldName])
-      var errorText = errorObj[fieldName].join(';')
-      var $errorDiv = $('<div>').addClass('error')
-      $errorDiv.html(fieldName + " - " + errorText)
-      $('form.signinform .signuperror').append($errorDiv)
-      }
-    })
-	}) // end signin post
+//       localStorage.setItem('apiToken', response['apiToken'])
+//       localStorage.setItem('currentUser', requestBody['username'])
+//       console.log("localstorage:",localStorage)
+      // $('.loggedin').append('Welcome, ' + localStorage.getItem("currentUser"))
+    // })
+    // .fail(function(error) {
+    //   console.log("Invalid POST request:", error.responseText)
+    //   var errorObj = JSON.parse(error.responseText)
+    //   for (var fieldName in errorObj) {
+    //   console.log(fieldName, errorObj[fieldName])
+    //   var errorText = errorObj[fieldName].join(';')
+    //   var $errorDiv = $('<div>').addClass('error')
+    //   $errorDiv.html(fieldName + " - " + errorText)
+    //   $('form.signinform .signuperror').append($errorDiv)
+    //   }
+    // })
+	// }) // end signin post
 }) //end document
  //localStorage.setItem('myKey', 'myVal') //(must be a string)
 
